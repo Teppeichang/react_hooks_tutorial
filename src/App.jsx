21973b7ff7876a78d4ebe09d220f2d9280e9cfb6
Teppei Torii <teppeichan9@gmail.com>
2022-11-23 10:29:52 +0900

    Add: react-router-dom, pageファイル

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome To React Hooks Tutorial</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
