37eca5967cb2061f7362ddab72e51435895e94c2
Teppei Torii <teppeichan9@gmail.com>
2022-12-03 14:01:05 +0900

    Feat: Topページ、useEffect解説ページ（ガワだけ）

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Top from "./pages/Top";
 import UseStatePage from "./pages/UseStatePage";
+import UseEffectPage from "./pages/UseEffectPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="flex flex-col">
         <p>React Hooks Tutorial</p>
+        <Link to="/">Top</Link>
         <Link to="/usestate">useState解説</Link>
-        <p>useEffect解説</p>
+        <Link to="/useeffect">useEffect解説</Link>
       </div>
       <Routes>
+        <Route path="/" element={<Top />} />
         <Route path="/usestate" element={<UseStatePage />} />
+        <Route path="/useeffect" element={<UseEffectPage />} />
       </Routes>
     </BrowserRouter>
   );
