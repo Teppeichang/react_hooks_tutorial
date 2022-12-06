3f32ccc1aa4990323e7d7145e567e1243da8d146
Teppei Torii <teppeichan9@gmail.com>
2022-12-06 15:13:04 +0900

    Fix: useEffect解説ページのheight

--- src/pages/UseEffectPage.jsx
+++ src/pages/UseEffectPage.jsx
@@ -1,6 +1,6 @@
 const UseEffectPage = () => {
   return (
-    <div className="my-20">
+    <div className="my-20 flex flex-col justify-center items-center min-h-screen">
       <h1>Coming soon...</h1>
     </div>
   )
