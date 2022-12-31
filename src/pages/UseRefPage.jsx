73014785056882e40b28cc16dd896bcc0ea4fbd3
Teppei Torii <teppeichan9@gmail.com>
2022-12-31 10:25:51 +0900

    Add: useRef解説ページに動作実例を追加

--- src/pages/UseRefPage.jsx
+++ src/pages/UseRefPage.jsx
@@ -9,23 +9,24 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Box } from "@mui/system";
 
 const UseRefPage = () => {
+  const emailValidatePattern = "^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$";
+
+  const emailRef = useRef(null);
   const textRef = useRef(null);
+  const [useRefText, setUseRefText] = useState(null);
+  const [useStateText, setUseStateText] = useState(null);
 
-  console.log(typeof textRef);
-  console.log(textRef);
-  console.log(textRef.current);
-
-  const [textError, setTextError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
 
   const formValidation = () => {
     let valid = true;
 
-    const isTextRefExists = textRef?.current;
-    if (isTextRefExists) {
-      const isTextValid = isTextRefExists.validity.valid;
-      setTextError(!isTextValid);
+    const isEmailRefExists = emailRef?.current;
+    if (isEmailRefExists) {
+      const isEmailValid = isEmailRefExists.validity.valid;
+      setEmailError(!isEmailValid);
       // eslint-disable-next-line
-      valid &&= isTextValid;
+      valid &&= isEmailValid;
     }
   };
 
@@ -34,17 +35,18 @@ const UseRefPage = () => {
       <p className="text-2xl mb-10">useRef</p>
       <div className="flex flex-col items-center mt-5 mb-20">
         <TextField
-          label="Input text"
-          inputRef={textRef}
-          error={textError}
-          helperText={textError && textRef?.current?.validationMessage}
-          inputProps={{ required: true }}
+          label="Email"
+          inputRef={emailRef}
+          error={emailError}
+          helperText={emailError && emailRef?.current?.validationMessage}
+          inputProps={{ required: true, pattern: emailValidatePattern }}
+          sx={{ m: 1 }}
         />
         <button
           className="bg-black hover:bg-slate-700 text-white font-medium mt-5 py-2 px-4 rounded w-max"
           onClick={() => formValidation()}
         >
-          Check Text
+          Submit
         </button>
       </div>
       <Box sx={{ width: 800, mb: 10 }}>
@@ -86,8 +88,7 @@ const UseRefPage = () => {
                 useRefは、.currentプロパティが渡された引数をrefObject(上記例における、hogeRefやtextRef)へ返す。
               </p>
               <p>
-                この引数の値が書き換え可能な.currentプロパティーの値であり、
-                .currentプロパティ内に保持することができる。
+                この引数の値が書き換え可能な.currentプロパティの値であり、.currentプロパティ内に保持することができる。
               </p>
             </Typography>
           </AccordionDetails>
@@ -101,19 +102,45 @@ const UseRefPage = () => {
               <p>値の保持ができると言う点では共通しているが、以下の点が異なる。</p>
               <ul className="list-disc list-inside pl-2 mt-5 leading-8">
                 <li>useState: 保持した値を更新すると、コンポーネントが再レンダリングされる</li>
-                <li>useRef: 保持した値を更新しても、<span className="font-bold underline">コンポーネントが再レンダリングされない</span></li>
+                <li>
+                  useRef: 保持した値を更新しても、
+                  <span className="font-bold underline">
+                    コンポーネントが再レンダリングされない
+                  </span>
+                </li>
               </ul>
+              <TextField
+                label="useState Form"
+                value={useStateText}
+                onChange={(event) => setUseStateText(event.target.value)}
+                sx={{ mt: 5, mx: 1 }}
+              />
+              <p className="ml-1 mb-5">Text: {useStateText}</p>
+              <TextField label="useRef Form" inputRef={textRef} sx={{ m: 1 }} />
+              <button
+                className="bg-black hover:bg-slate-700 text-white font-medium mt-5 py-2 px-4 rounded w-max"
+                onClick={() => setUseRefText(textRef.current.value)}
+              >
+                Set Text
+              </button>
+              <p className="ml-1 mb-5">Text: {useRefText}</p>
             </Typography>
           </AccordionDetails>
         </Accordion>
         <Accordion>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography>useStateとの棲み分け</Typography>
+            <Typography>useStateとの使い分け</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              <p>「コンポーネント内で値を保持したいが、ブラウザ上に更新した内容をリアルタイムで表示させる必要がない」場合は、useRefを利用するとよい。</p>
-              <p>例えば、フォームのバリデーションエラーはリアルタイムで表示させることがマストではないので、useRefでフォームの値を保持しておいて、フォーム送信時にuseRefで保持したフォームの値を検証し、バリデーションエラーを表示させる というような使い方ができる。</p>
+              <p>
+                「コンポーネント内で値を保持したいが、ブラウザ上に更新した内容をリアルタイムで表示させる必要がない」場合は、useRefを利用するとよい。
+              </p>
+              <br />
+              <p>
+                例えば、フォームのバリデーションエラーはリアルタイムで表示させることがマストではないので、useRefでフォームの値を保持しておいて、フォーム送信時にuseRefで保持したフォームの値を検証し、バリデーションエラーを表示させる
+                というような使い方ができる。
+              </p>
             </Typography>
           </AccordionDetails>
         </Accordion>
