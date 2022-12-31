0919b4a73e40657105b130f35f270c065882d4f7
Teppei Torii <teppeichan9@gmail.com>
2022-12-31 11:38:03 +0900

    Fix: フッターのリンクのセキュリティリスク(rel=noreferrerの追加)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-black bottom-0">
       <div className="flex justify-center items-center text-white p-5 w-screen">
         ©Copyright Teppei Torii 2022 All Rights Reserved
-        <a href="https://github.com/Teppeichang/react_hooks_tutorial" target="_blank" className="ml-5"><FaGithub /></a>
+        <a href="https://github.com/Teppeichang/react_hooks_tutorial" target="_blank" rel="noreferrer" className="ml-5"><FaGithub /></a>
       </div>
     </div>
   )
