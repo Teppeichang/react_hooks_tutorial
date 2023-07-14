ae1cfb905b565eaaf7457e6f01c3c9345ad68026
Teppeichang <teppeichan9@gmail.com>
2023-07-14 12:00:06 +0900

    Fix: メモ化に関する解説文を用語集(メモ化)ni

--- src/pages/glossaries/Memorize.jsx
+++ src/pages/glossaries/Memorize.jsx
@@ -1,9 +1,21 @@
 const Memorize = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <p>Coming soon...</p>
+    <div className="my-20 p-10 flex flex-col min-h-screen">
+      <p className="text-2xl font-bold">メモ化とは</p>
+      <hr />
+      <p className="mt-5">
+        関数の呼び出し結果(=値)をキャッシュ(記憶)しておき、再度同じ関数の呼び出しが発生した際に、
+        <span className="font-bold">
+          同じ関数を再度実行するのではなく、キャッシュから関数の呼び出し結果を返す
+        </span>
+        こと。
+      </p>
+      <p className="text-2xl font-bold">メモ化による効果</p>
+      <hr />
+      <p className="mt-5">不要な関数の再実行や、再レンダリングの発生を抑えることができる。</p>
+      <p></p>
     </div>
-  )
-}
+  );
+};
 
 export default Memorize;
